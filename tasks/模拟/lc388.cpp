ae467a807e43d40368b2c3ee64f6fc137b03fd63
GWrety <2643782286@qq.com>
2022-04-20 10:33:11 +0800

    22.4.20

---
+++ tasks/模拟/lc388.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int lengthLongestPath(string input) {
+        vector<pair<int,int>> wit;
+        int ans=0;
+        int n=input.size();
+        int len=0;
+        int start=0,end=-1;
+        for(int i=0;i<=n;++i){
+            if(i==n||input[i]=='\n'){
+                int size=end-start+1;
+                if(is_file(input,start,end)){
+                    while(!wit.empty()&&wit.back().first>=len) wit.pop_back();
+                    int temp=0;
+                    for(auto x:wit){
+                        temp+=x.second;
+                        temp++;
+                    }
+                    temp+=size;
+                    ans=max(ans,temp);
+                }
+                else{
+                    while(!wit.empty()&&wit.back().first>=len) wit.pop_back();
+                    wit.push_back(make_pair(len,size));
+                }
+                len=0;
+                start=i+1;
+            }
+            else if(input[i]=='\t'){
+                len++;
+                start++;
+            }
+            else{
+                end=i;
+            }
+        }
+        return ans;
+    }
+    bool is_file(string& input,int start,int end){
+        for(int i=start;i<=end;++i){
+            if(input[i]=='.'){
+                return true;
+            }
+        }
+        return false;
+    }
+};
