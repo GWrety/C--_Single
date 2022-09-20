2351d4ca55fe88f748b7fe743cd117c45d34c3c9
GWrety <2643782286@qq.com>
2022-09-20 16:16:23 +0800

    9.20

---
+++ leetcode每日一题/22.9.20!.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//能否将nums分成k个集合,使其总和都相等
+//每个集合和为 sum/k  如果除不尽 false
+//想不到比较好的解法
+//! dp还是难啊
+//状态压缩加记忆化搜索
+class Solution {
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int all = accumulate(nums.begin(), nums.end(), 0);
+        if (all % k > 0) {
+            return false;
+        }
+        int per = all / k; 
+        sort(nums.begin(), nums.end());
+        if (nums.back() > per) {
+            return false;
+        }
+        int n = nums.size();
+        vector<bool> dp(1 << n, true);
+        function<bool(int,int)> dfs = [&](int s, int p)->bool {
+            if (s == 0) {
+                return true;
+            }
+            if (!dp[s]) {
+                return dp[s];
+            }
+            dp[s] = false;
+            for (int i = 0; i < n; i++) {
+                if (nums[i] + p > per) {
+                    break;
+                }
+                if ((s >> i) & 1) {
+                    if (dfs(s ^ (1 << i), (p + nums[i]) % per)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        };
+        return dfs((1 << n) - 1, 0);
+    }
+};
+
+///状态压缩+动态规划
+class Solution {
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int all = accumulate(nums.begin(), nums.end(), 0);
+        if (all % k > 0) {
+            return false;
+        }
+        int per = all / k; 
+        sort(nums.begin(), nums.end());
+        if (nums.back() > per) {
+            return false;
+        }
+        int n = nums.size();
+        vector<bool> dp(1 << n, false);
+        vector<int> curSum(1 << n, 0);
+        dp[0] = true;
+        for (int i = 0; i < 1 << n; i++) {
+            if (!dp[i]) {
+                continue;
+            }
+            for (int j = 0; j < n; j++) {
+                if (curSum[i] + nums[j] > per) {
+                    break;
+                }
+                if (((i >> j) & 1) == 0) {
+                    int next = i | (1 << j);
+                    if (!dp[next]) {
+                        curSum[next] = (curSum[i] + nums[j]) % per;
+                        dp[next] = true;
+                    }
+                }
+            }
+        }
+        return dp[(1 << n) - 1];
+    }
+};
+
+
+int main()
+{
+    
+    system("pause");
+    return 0;
+}
+
--- temp/tmep1.cpp
+++ temp/tmep1.cpp
@@ -7,4 +7,3 @@ int main()
     system("pause");
     return 0;
 }
-
