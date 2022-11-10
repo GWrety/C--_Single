4683317a8d66165b8adde0262940e3ce7c672bf7
GWrety <2643782286@qq.com>
2022-11-10 09:48:13 +0800

    11.20

--- temp/tmep1.cpp
+++ temp/tmep1.cpp
@@ -1,9 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
 int main()
 {
-    
+    int start=1952;
+    for(int time=start;true;time++){
+        if(time==2022){
+            cout<<("70周年快乐!");
+        }
+    }
     system("pause");
     return 0;
 }
+
